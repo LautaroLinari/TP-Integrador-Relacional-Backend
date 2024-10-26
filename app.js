d897828864cc705b1e6e4674045a96df1f70fbdd
LautaroLinari <lautarolinari18@hotmail.com>
2024-10-26 16:36:13 -0300

    2- instalando dependencias, modificando app.js y database.js

--- app.js
+++ app.js
@@ -2,14 +2,38 @@ const express = require('express');
 const app = express();
 const contenidoRoutes = require('./routes/contenidoRoutes');
 const db = require('./conexion/database');
+const { sequelize } = require('./conexion/database');
+
+
+sequelize.sync()
+  .then(() => {
+    console.log('Base de datos sincronizada.');
+  })
+  .catch((err) => {
+    console.error('Error al sincronizar la base de datos:', err);
+  })
+
 
 // Middlewares
 app.use(express.json());
-app.use('/contenido', contenidoRoutes);
+app.use('/', contenidoRoutes);
+
+
+// Middleware para manejar rutas inexistentes
+app.use((req, res, next) => {
+  res.status(404).json({ message: 'Ruta no encontrada' });
+})
+
+
+// Middleware de manejo de errores
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ message: 'Ocurrió un error en el servidor', details: err.message });
+})
+
 
 // Server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
-    
+  console.log(`Server running on http://localhost:${PORT}`);
+})
